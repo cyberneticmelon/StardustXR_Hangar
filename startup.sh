5761a65e4ac614a6c995648c835dcb8b8bccd905
Cyberneticmelon <cyberneticxmelon@gmail.com>
2025-05-22 01:05:13 -0400

    Updated justfile recipies and startup file

--- startup.sh
+++ startup.sh
@@ -3,9 +3,9 @@
 xwayland-satellite :10 &
 export DISPLAY=:10
 
-./prefix/bin/hexagon_launcher &
-./prefix/bin/flatland &
-./prefix/bin/atmosphere show the_grid &
-./prefix/bin/client-template
+./protostar/target/debug/hexagon_launcher &
+./flatland/target/debug/flatland &
+./atmosphere/target/debug/atmosphere show the_grid &
+./gravity/target/debug/gravity 0.7 0 0 ./client-template/target/debug/client-template &
 
-./prefix/bin/gravity 0 0 -0.5 konsole
+WAYLAND_DISPLAY=$FLAT_WAYLAND_DISPLAY ./non-spatial-input/target/debug/manifold | ./non-spatial-input/target/debug/simular &
